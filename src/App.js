da8620877c9ae4185b4aabd961ce5a04f5a16b5f
Shalom Alalade <alaladeshalom02@gmail.com>
2025-11-13 15:58:23 +0000

    define search function that makes network request to OpenWeatherMap API, converts API response to JavaScript object and parse JSON object from the API

--- src/App.js
+++ src/App.js
@@ -12,6 +12,18 @@ function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}/weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+    });
+  }
+}
+
   //function that tells today's date
   const todaysDate = (entry) => {
     
@@ -26,7 +38,8 @@ function App() {
     <div className="app">
       <main>
         <div className='search-box'>
-          <input type="text" className='search-bar' placeholder='Search here ...'></input>
+          <input type="text" className='search-bar' placeholder='Search here ...' 
+          onChange={(e) => setQuery(e.target.value)} value={query} onKeyDown={search}></input>
         </div>
         <div className='weather-display'>
           <div className='location'> Osogbo City, NGR</div>
