a9dbf7cab5b6c923fb3e6a36400cc2ba3c9cac70
Shalom Alalade <alaladeshalom02@gmail.com>
2025-11-20 22:26:26 +0000

    add pages about.js, home.js and styles about.css and home.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import About from "./pages/About";
+import HomePage from "./pages/Weather";
+import About from "./pages/about";
+import NavBar from "./components/NavBar";
+import Weather from "./pages/Weather";
+import Welcome from "./pages/Home";
+import { queryByAttribute } from "@testing-library/dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
+  <>
+    <NavBar/>
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/weather" element={<Weather />} />
       <Route path="/about" element={<About />} />
     </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+      <div className='home-container'>
+        <h1 className='text'>Your Daily Weather App</h1>
+      </div>
+      
+    </>
+  );
+}
+
+export default Home;
