a1e69ef99ecf82a0d9439e14bc579835e473dc6f
Shalom Alalade <alaladeshalom02@gmail.com>
2025-11-25 14:50:39 +0000

    feat: add ScrollToTop component for smooth page navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route  } from "react-router-dom";
 import HomePage from "./pages/Weather";
 import About from "./pages/About";
 import NavBar from "./components/NavBar";
@@ -7,18 +7,20 @@ import Welcome from "./pages/Home";
 import { queryByAttribute } from "@testing-library/dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer"
+import ScrollToTop from "./components/scrollToTop";
 
 function App() {
   return (
-  <>
-    <NavBar/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/weather" element={<Weather />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-    <Footer/>
-    </>
+    <>
+      <ScrollToTop />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/weather" element={<Weather />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+   </>
   );
 }
 
